b23a09e180e0d0f0613967267c719eeac318a576
FMeacham <frazermeacham@gmail.com>
2017-09-07 09:48:23 -0700

    Words are good.

--- multiply.py
+++ multiply.py
@@ -3,4 +3,4 @@ import sys
 if __name__ == "__main__":
     args = [float(arg) for arg in sys.argv[1:]]
     (a, b) = args
-    print(a*b)
+    print('The product of {0} and {1} is'.format(a, b), a*b)
