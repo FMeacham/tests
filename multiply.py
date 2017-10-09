3ee7f9dcef4217dcb2ebc9674a3ada7f0a62fdf2
Frazer Meacham <frazermeacham@gmail.com>
2017-10-09 16:30:31 -0700

    Modified multiply.py and created gitlog1.1.txt

--- multiply.py
+++ multiply.py
@@ -4,3 +4,4 @@ if __name__ == "__main__":
     args = [float(arg) for arg in sys.argv[1:]]
     (a, b) = args
     print('The product of {0} and {1} is'.format(a, b), a*b)
+    print('Hello new branch!')
